816eda90c2a4d29f9c595ca3e3ea8ed08316060e
HoangVNH <huyhoangnguyen0897@gmail.com>
2020-04-03 16:47:38 +0700

    Create middleware to authenticate user

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ require('dotenv').config(); // for loading enviroment variables
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+const usersRoute = require('./routes/api/users');
+const postsRoute = require('./routes/api/posts');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
 
+// db configuration
 const MONGO_URL = process.env.MONGO_URL;
-
 mongoose
   .connect(MONGO_URL, {
     useNewUrlParser: true,
@@ -19,6 +26,9 @@ mongoose
   })
   .then(() => console.log('DB connected !!!'))
   .catch(err => console.log(err));
+
+app.use('/api/users', usersRoute);
+app.use('/api/posts', postsRoute);
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}`);
